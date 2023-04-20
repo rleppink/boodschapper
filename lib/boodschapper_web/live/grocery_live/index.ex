9bd81b304255023fcf487e1d4ba07f9415e87c59
Ronald Leppink <ronaldleppink@gmail.com>
2023-04-20 13:41:54 +0200

    Remove unused code

--- lib/boodschapper_web/live/grocery_live/index.ex
+++ lib/boodschapper_web/live/grocery_live/index.ex
@@ -2,7 +2,6 @@ defmodule BoodschapperWeb.GroceryLive.Index do
   use BoodschapperWeb, :live_view
 
   alias Boodschapper.Groceries
-  alias Boodschapper.Groceries.Grocery
 
   @topic inspect(__MODULE__)
 
@@ -12,36 +11,14 @@ defmodule BoodschapperWeb.GroceryLive.Index do
 
     {:ok,
      socket
+     |> assign(:page_title, "🛒 Boodschappen")
      |> stream(:groceries, Groceries.list_groceries())
      |> stream(:tags, Groceries.list_tags())}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Pas boodschap aan")
-    |> assign(:grocery, Groceries.get_grocery!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "Nieuwe boodschap")
-    |> assign(:grocery, %Grocery{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "🛒 Boodschappen")
-    |> assign(:grocery, nil)
-  end
-
-  defp apply_action(socket, :check, _params) do
-    socket
-    |> assign(:grocery, nil)
+    {:noreply, socket}
   end
 
   @impl true
@@ -53,16 +30,6 @@ defmodule BoodschapperWeb.GroceryLive.Index do
     )
 
     {:noreply, stream_insert(socket, :groceries, grocery)}
-  end
-
-  @impl true
-  def handle_info({Boodschapper.PubSub, :saved, grocery}, socket) do
-    {:noreply, stream_insert(socket, :groceries, grocery)}
-  end
-
-  @impl true
-  def handle_info({Boodschapper.PubSub, :deleted, grocery}, socket) do
-    {:noreply, stream_delete(socket, :groceries, grocery)}
   end
 
   def handle_info(:clear_flash, socket) do
@@ -89,7 +56,7 @@ defmodule BoodschapperWeb.GroceryLive.Index do
   end
 
   defp save_grocery(socket, :new, grocery_params) do
-    {:ok, grocery} = Groceries.create_grocery(grocery_params) |> IO.inspect(label: "grocery")
+    {:ok, grocery} = Groceries.create_grocery(grocery_params)
 
     Phoenix.PubSub.broadcast(
       Boodschapper.PubSub,
